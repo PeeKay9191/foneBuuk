813d85b2e2fd740a67e105c8a0c651a9a167b682
prince <pkbaffoe9191@gmail.com>
2024-07-26 18:40:32 +0000

    Added the login form imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import { LoginForm } from "@/components/login_form";
 
 export default function Home() {
   return (
-   <div className="flex items-center justify-center h-screen" >
-    <LoginForm />
-   </div>
+    <div className="flex items-center justify-center h-screen">
+      <LoginForm />
+    </div>
   );
 }
