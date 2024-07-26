93f126ceb36373fc3f63fdce0d654d2344c9dd2f
prince <pkbaffoe9191@gmail.com>
2024-07-26 16:28:15 +0000

    Initial Commmits

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex  items-center justify-center p-24">
+        <h1 className="text-3xl font-bold text-gray-900">Welcome to my Golden Book Exchange</h1>
+    </main>
+  );
+}
