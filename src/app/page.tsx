2cda6ce46ca8966a422e3226aeba333b3f711c05
prince <pkbaffoe9191@gmail.com>
2024-07-26 18:35:24 +0000

    Setting up auth pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
+import { LoginForm } from "@/components/login_form";
 
 export default function Home() {
   return (
-    <main className="flex  items-center justify-center p-24">
-        <h1 className="text-3xl font-bold text-gray-900">Welcome to my Golden Book Exchange</h1>
-    </main>
+   <div className="flex items-center justify-center h-screen" >
+    <LoginForm />
+   </div>
   );
 }
